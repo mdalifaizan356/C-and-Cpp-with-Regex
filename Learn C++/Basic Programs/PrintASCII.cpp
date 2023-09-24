18e549638f0140f3f8d34e9ba56b09e133e96bd0
faizanpasha356 <mdalifaizan356@gmail.com>
2023-09-24 23:54:30 +0530

    Clear Array Concept

---
+++ Learn C++/Basic Programs/PrintASCII.cpp
@@ -0,0 +1,11 @@
+// Print ASCII VALUE
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char mychar;
+    cout<<"Enter any number or character for find this ascii value\n";
+    cin>>mychar;
+    cout<<"ASCII Value of "<<mychar<<" is\t"<<int(mychar);
+
+    return 0;
+}
---
+++ Learn C++/Basic Programs/Tempreturecalculate.cpp
@@ -0,0 +1,26 @@
+// //C++ Program to Calculate Fahrenheit to Celsius
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     float tempinfrnht,convert1;
+//     cout<<"Enter temperature in Fahrenheit \n";
+//     cin>>tempinfrnht;
+//     convert1 = (tempinfrnht - 32.0) * 5.0 / 9.0;
+//     cout<<"Calculate Fahrenheit to Celsius "<<convert1<<" celsius";
+
+//     return 0;
+// }
+
+
+//C++ Program to Calculate Celsius to Fahrenheit
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float tempincelsius,convert2;
+    cout<<"Enter temperature in Fahrenheit \n";
+    cin>>tempincelsius;
+    convert2 = (tempincelsius - 32.0) * 5.0 / 9.0;
+    cout<<"Calculate Fahrenheit to Celsius "<<convert2<<" celsius";
+
+    return 0;
+}
