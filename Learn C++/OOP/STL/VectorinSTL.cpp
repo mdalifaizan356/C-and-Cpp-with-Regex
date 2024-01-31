91547cd626e914920cc82afead8db43f234fb59c
faizanpasha356 <mdalifaizan356@gmail.com>
2024-01-31 21:24:46 +0530

    Learn Vactor

--- Learn C++/OOP/STL/VectorinSTL.cpp
+++ Learn C++/OOP/STL/VectorinSTL.cpp
@@ -10,16 +10,38 @@ Vector runtime pr size increase karta he.
 Vector being a dynamic array, doesn't needs size during declaration.
 
 subscript operator[] is also defined for vector.
+push_back(): it is a member function, which can be used to add value to the vector at the end.
 
 */
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     vector<int>v0{2,3,4,5};                 //Initialize value during declaration
+//     vector<int>v1;                         //create a blank vector
+//     vector<int>v2(4);                     //create a vector with size
+//     vector<string>v3{"Faizan","Pasha"};  //Initialize during declaration
+//     vector<int>v4(5,10);      //Initialize same value in all block during declaration with size
+//     for(int i=0; i<4; i++){
+//     cout<<v4[0]<<endl;
+//     }
+// return 0;
+// }
 
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int>v1;                         //create a blank vector
-    vector<int>v2(4);                     //create a vector with size
-    vector<string>v3{"Faizan","Pasha"};  //Initialize during declaration
-    vector<int>v4(5,10);      //Initialize same value in all block during declaration with size
+    vector<int>v1;
+    cout<<v1.capacity()<<endl;  //Now capacity is 0;
+    v1.push_back(10);
+    cout<<v1.capacity()<<endl;  //Now capcity is 1;
+    v1.push_back(20);
+    cout<<v1.capacity()<<endl;  //Now capcity is 2;
+    v1.push_back(30);
+    cout<<v1.capacity()<<endl;  //Now capcity is 4;
 
+    v1.pop_back();
+    cout<<v1.capacity()<<endl;  //Now capcity is 1;
+    
 return 0;
 }
